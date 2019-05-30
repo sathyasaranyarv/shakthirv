97f9c01cb73d15582275258c770bf75a6e040ac6
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-30 10:43:11 +0530

    Add files via upload

---
+++ Series.java
@@ -0,0 +1,22 @@
+class Series
+{
+public static void main(String args[])
+{
+int limit=9;
+int num=8;
+for(int i=1;i<=9;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+for(int j=1;j<=8;j++)
+{
+if(j%2==0)
+{
+System.out.println(j);
+}
+}
+}
+}
