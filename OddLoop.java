44de3fbf2481f779c068120b1214d7f8bb5accec
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-30 10:17:10 +0530

    Add files via upload

---
+++ OddLoop.java
@@ -0,0 +1,15 @@
+class OddLoop
+{
+public static void main(String args[])
+{
+int i=20;
+System.out.println("print the Odd numbers between 1 to 20");
+for(i=1;i<20;i++)
+{
+if(i%2 !=0)
+{
+System.out.println(i);
+}
+}
+}
+}
