9dbc8449d3f85852685d4c60a8dd3f9848cb72f4
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-30 10:18:23 +0530

    Add files via upload

---
+++ EvenLoop.java
@@ -0,0 +1,14 @@
+class EvenLoop
+{
+public static void main(String args[])
+{
+int limit=20;
+System.out.println("printing even numbers between 2 to 20");
+for(int i=1;i<=limit;i++)
+{
+if(i%2==0){
+System.out.println(i);
+}
+}
+}
+}
